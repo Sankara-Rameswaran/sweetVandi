2bfbc81831c245afe56571679737c48ba4641519
Sankar <sankara.rameswaran@utvyakta.com>
2026-07-18 19:36:43 +0530

    Implemented rate limiter for auth routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,14 +10,25 @@ const healthRoutes = require('./routes/health.routes');
 const authRoutes = require('./routes/auth.routes');
 const productRoutes = require('./routes/product.routes');
 const cartRoutes = require('./routes/cart.routes');
-
+const rateLimit = require('express-rate-limit');
 const app = express();
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 2,
+    message: {
+        error: 'Too many requests. Please try again later.',
+    },
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(morganWinston);
 app.use('/uploads', express.static('uploads'));
 app.use('/api/v1/', healthRoutes);
-app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/auth', limiter, authRoutes);
 app.use('/api/v1/category', categoryRoutes);
 app.use('/api/v1/product', productRoutes);
 app.use('/api/v1/cart', cartRoutes);
